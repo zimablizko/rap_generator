ceb1cd0de63215dc053962220b0540a8fcfa055f
maggina <blu.bl@ya.ru>
2018-11-03 13:13:55 +0300

    много чего

--- src/test/java/SentensesTest.java
+++ src/test/java/SentensesTest.java
@@ -2,6 +2,8 @@ import DB.DBClient;
 import Models.Lemma;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 
 public class SentensesTest {
 
@@ -22,8 +24,14 @@ public class SentensesTest {
     public void testGetSentenceByTemplate() {
         //Sentenses sn=new Sentenses();
         //Lemma noun = Lemma.noun(1,1,1,1,0);
-        String sentense= DBClient.getSentenceByTemplate(5);
+        String sentense= DBClient.getSentenceByTemplate(10);
         System.out.println(sentense);
+        String sentense2= DBClient.getSentenceByTemplate(11);
+        System.out.println(sentense2);
+        String sentense3= DBClient.getSentenceByTemplate(12);
+        System.out.println(sentense3);
+        String sentense4= DBClient.getSentenceByTemplate(13);
+        System.out.println(sentense4);
     }
 
     @Test
@@ -34,5 +42,36 @@ public class SentensesTest {
         System.out.println(str.replaceAll("/n",
                 "\n"));
         System.out.println(sentense);
+    }
+
+
+    @Test
+    public void testGetSyllableCount() {
+        //Sentenses sn=new Sentenses();
+        //Lemma noun = Lemma.noun(1,1,1,1,0);
+        int count= DBClient.getSyllableCount("И твердь не всаживает");
+        System.out.println(count);
+    }
+    @Test
+    public void setStressTest() {
+        DBClient.setStress();
+        System.out.println();
+    }
+    @Test
+    public void setSyllableCountTest() {
+        DBClient.setSyllableCount();
+        System.out.println();
+    }
+
+    @Test
+    public void getCSVforStressTest() throws IOException {
+        DBClient.getCSVforStress();
+        System.out.println();
+    }
+
+    @Test
+    public void writeStressFromCSVTest() throws IOException {
+        DBClient.writeStressFromCSV();
+        System.out.println();
     }
 }
